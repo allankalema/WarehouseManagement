6a256515f2a0a2058595ae6197eee5c7b288d1f0
allankalema <allankalema4@gmail.com>
2024-11-05 12:32:47 +0300

    email sending has been enabled

--- warehouse/warehouse/settings.py
+++ warehouse/warehouse/settings.py
@@ -124,3 +124,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'theplanhub9@gmail.com'  # Replace with your email address
+EMAIL_HOST_PASSWORD = 'pjry zeic txwc qdhl'  # Replace with your email password
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
