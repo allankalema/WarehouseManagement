f6656d2f4b0156e9752cbe0a83500f4099b86987
allankalema <allankalema4@gmail.com>
2024-11-06 17:05:46 +0300

    making adjustments on products

--- warehouse/products/views.py
+++ warehouse/products/views.py
@@ -1,7 +1,10 @@
 # products/views.py
 from django.shortcuts import render
 from .models import Product
+from user.decorators import *
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def product_list(request):
     products = Product.objects.all()  # Fetch all products
     return render(request, 'products/product_list.html', {'products': products})
