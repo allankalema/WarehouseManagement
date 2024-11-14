7e3be92f73547535c086aa2c95f70047ffc14c61
SENYONJO HENRY <senyonjohenry1@gmail.com>
2024-11-14 14:56:34 +0300

    made some changes in the base file

--- warehouse/user/forms.py
+++ warehouse/user/forms.py
@@ -3,11 +3,20 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 class UserRegistrationForm(UserCreationForm):
-    username = forms.CharField(required=True)
-    first_name = forms.CharField(required=True)
-    last_name = forms.CharField(required=True)
-    email = forms.EmailField(required=True)
-    store_name = forms.CharField(required=True, label="Store Name")  # Changed field name to store_name
+    # username = forms.CharField(required=True)
+    # first_name = forms.CharField(required=True)
+    # last_name = forms.CharField(required=True)
+    # email = forms.EmailField(required=True)
+    # store_name = forms.CharField(required=True, label="Store Name")  # Changed field name to store_name
+
+    username = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    store_name = forms.CharField(required=True, label="Store Name", widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
 
     class Meta:
         model = User
